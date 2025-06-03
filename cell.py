63a2c55e92b6171cdc1569d99ca24c09f1a1e0e1
Nick Lanzo <nlanzo@gmail.com>
2025-06-03 13:57:37 -0500

    add functionality to draw path from one cell to another

--- cell.py
+++ cell.py
@@ -13,10 +13,21 @@ class Cell():
         self.__win = window
 
     def draw(self, x1, y1, x2, y2):
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        if self.__win is None:
+            return
+        if x2 >= x1:
+            self.__x1 = x1
+            self.__x2 = x2
+        else:
+            self.__x1 = x2
+            self.__x2 = x1
+        if y2 >= y1:
+            self.__y1 = y1
+            self.__y2 = y2
+        else:
+            self.__y1 = y2
+            self.__y2 = y1
+
         if self.has_left_wall:
             left_wall = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(left_wall)
@@ -29,3 +40,20 @@ class Cell():
         if self.has_bottom_wall:
             bottom_wall = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(bottom_wall)
+
+
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+        from_x, from_y = self.get_midpoint()
+        to_x, to_y = to_cell.get_midpoint()
+        point_from = Point(from_x, from_y)
+        point_to = Point(to_x, to_y)
+        self.__win.draw_line(Line(point_from, point_to), color)
+
+
+    def get_midpoint(self):
+        mid_x = (self.__x2 + self.__x1) // 2
+        mid_y = (self.__y2 + self.__y1) // 2
+        return mid_x, mid_y
--- main.py
+++ main.py
@@ -6,8 +6,16 @@ def main():
     win = Window(800, 600)
     cell1 = Cell(win)
     cell2 = Cell(win)
+    cell3 = Cell(win)
+    cell1.has_bottom_wall = False
+    cell2.has_top_wall = False
+    cell3.has_right_wall = False
+    cell3.has_left_wall = False
     cell1.draw(50, 50, 150, 150)
-    cell2.draw(500, 500, 400, 400)
+    cell2.draw(50, 200, 250, 250)
+    cell3.draw(390, 390, 300, 300)
+    cell1.draw_move(cell2)
+    cell2.draw_move(cell3, undo=True)
     win.wait_for_close()
 
 
