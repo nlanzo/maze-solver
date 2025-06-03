aca955c330e57bfa02cd632fd67b1af75c72d130
Nick Lanzo <nlanzo@gmail.com>
2025-06-03 15:20:35 -0500

    add tests for maze creation

---
+++ tests.py
@@ -0,0 +1,52 @@
+import unittest
+from maze import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_dimension_valid(self):
+        num_cols = 14
+        num_rows = 11
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_rows,
+        )
+        self.assertEqual(
+            m1._Maze__cells[0][0].has_left_wall,
+            True,
+        )
+        self.assertEqual(
+            m1._Maze__cells[num_cols - 1][num_rows - 1].has_right_wall,
+            True,
+        )
+        self.assertEqual(
+            m1._Maze__cells[0][0].has_top_wall,
+            True,
+        )
+        self.assertEqual(
+            m1._Maze__cells[num_cols - 1][num_rows - 1].has_bottom_wall,
+            True,
+        )
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
